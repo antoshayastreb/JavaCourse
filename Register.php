bc4c8da02760f26a2635fa91e256cf0bbd394c32
Alexander Aldokhin <Delphideveloper@mail.ru>
2020-03-02 17:42:38 +0300

    Исправил Register.php

--- Register.php
+++ Register.php
@@ -14,12 +14,12 @@
 <?php
     require ('connect.php');
 
-    if (isset($_POST['email']) && isset($_POST['password'])){
-        $email = $_POST['email'];
-        $user_firstname = $_POST['user_fistname'];
+if (isset($_POST['inputEmail']) && isset($_POST['inputPassword'])){
+        $email = $_POST['inputEmail'];
+        $user_firstname = $_POST['user_firstname'];
         $user_lasttname = $_POST['user_lastname'];
         $user_partname = $_POST['user_parname'];
-        $password = $_POST['password'];
+        $password = $_POST['inputPassword'];
         $pass_hash = password_hash($password, PASSWORD_DEFAULT);
         $is_teacher = 0;
 
@@ -28,29 +28,27 @@
         $result = mysqli_query($connection, $quary);
 
         if ($result){
-            $smsg = "Регисрация прошла успешно";
+            $smsg = "Регистрация прошла успешно";
         } else {
             $fsmsg = "Ошибка";
         }
     }
 ?>
-<form class="form-singup" method="POST">
+<form class="form-singup" method="POST" action="Register.php">
     <img class="mb-4" src="./assets/jde.png" alt="" width="72" height="72">
     <h1 class="h3 mb-3 font-weight-normal">Регистрация</h1>
     <?php if(isset($smsg)){?><div class="alert-success" role="alert"><?php echo $smsg; ?></div><?php }?>
     <?php if(isset($fsmsg)){?><div class="alert-danger" role="alert"><?php echo $fsmsg; ?></div><?php }?>
     <label for="inputEmail" class="sr-only">Email адрес</label>
-    <input type="email" id="inputEmail" class="form-control" placeholder="Email адрес" required autofocus>
+    <input type="email" name="inputEmail" class="form-control" placeholder="Email адрес" required autofocus>
     <label for="inputFirstName" class="sr-only">Имя</label>
-    <input type="user_firstname" id="user_firstname" class="form-control" placeholder="Имя" required autofocus>
+    <input type="user_firstname" name="user_firstname" class="form-control" placeholder="Имя" required autofocus>
     <label for="inputLastName" class="sr-only">Фамилия</label>
-    <input type="user_lasttname" id="user_lastname" class="form-control" placeholder="Фамилия" required autofocus>
+    <input type="user_lasttname" name="user_lastname" class="form-control" placeholder="Фамилия" required autofocus>
     <label for="inputParName" class="sr-only">Отчество</label>
-    <input type="user_parname" id="user_parname" class="form-control" placeholder="Отчество" required autofocus>
+    <input type="user_parname" name="user_parname" class="form-control" placeholder="Отчество" required autofocus>
     <label for="inputPassword" class="sr-only">Пароль</label>
-    <input type="password" id="inputPassword" class="form-control" placeholder="Пароль" required>
-
-
+    <input type="password" name="inputPassword" class="form-control" placeholder="Пароль" required>
     <button class="btn btn-lg btn-primary btn-block" type="submit">Зарегистрироваться</button>
     <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
 </form>
