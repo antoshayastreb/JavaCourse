7f019a333e326d5d8277e89bbec76820684e9584
Alexander Aldokhin <Delphideveloper@mail.ru>
2020-03-03 22:41:23 +0300

    Переделка под новую базу и PDO

--- Connect.php
+++ Connect.php
@@ -1,5 +1,10 @@
 <?php
-    $connection = mysqli_connect('localhost', 'root');
-    $select_db = mysqli_select_db($connection, 'javacourse');
-
+    //$connection = mysqli_connect('localhost', 'root');
+    //$select_db = mysqli_select_db($connection, 'javacourses');
+try {
+    $db = new PDO('mysql:dbname=javacourses;host=localhost',
+        'root','');
+} catch (PDOException $e) {
+    print "Couldn't connect to the database: " . $e->getMessage();
+}
 ?>
--- Register.php
+++ Register.php
@@ -23,15 +23,15 @@ if (isset($_POST['inputEmail']) && isset($_POST['inputPassword'])){
         $pass_hash = password_hash($password, PASSWORD_DEFAULT);
         $is_teacher = 0;
 
-        $quary = "INSERT INTO users (user_firstname, user_lastname, user_parname, email, pass_hash, is_teacher) VALUES ('$user_firstname', '$user_lasttname', '$user_parname',
-        '$email', '$pass_hash', '$is_teacher')";
-        $result = mysqli_query($connection, $quary);
+    /*$quary = "INSERT INTO users (EMAIL, FirstName, LastName, patronymic, InGroup, teacher, stage, registered, pass_hash) VALUES ('$email', '$user_firstname', '$user_lasttname', '$user_parname',
+    , '$pass_hash')";
+    $result = mysqli_query($connection, $quary);
 
-        if ($result){
-            $smsg = "Регистрация прошла успешно";
-        } else {
-            $fsmsg = "Ошибка";
-        }
+    if ($result){
+        $smsg = "Регистрация прошла успешно";
+    } else {
+        $fsmsg = "Ошибка";
+    }*/
     }
 ?>
 <form class="form-singup" method="POST" action="Register.php">
@@ -39,16 +39,39 @@ if (isset($_POST['inputEmail']) && isset($_POST['inputPassword'])){
     <h1 class="h3 mb-3 font-weight-normal">Регистрация</h1>
     <?php if(isset($smsg)){?><div class="alert-success" role="alert"><?php echo $smsg; ?></div><?php }?>
     <?php if(isset($fsmsg)){?><div class="alert-danger" role="alert"><?php echo $fsmsg; ?></div><?php }?>
+    <label for="csGroup">Группа</label>
+    <select class="form-control" name="csGroup">
+        <?php
+        $sth = $db->prepare("SELECT * FROM jc_groups");
+        $sth->execute();
+        $array = $sth->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($array as $key => $value) {
+            print "<option value=\"$value[ID]\">$value[Name]</option>";
+        }
+        ?>
+    </select>
+    <label for="csGroup">Преподаватель</label>
+    <select class="form-control" name="csTeacher">
+        <?php
+        $sth = $db->prepare("SELECT * FROM jc_teachers");
+        $sth->execute();
+        $array = $sth->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($array as $key => $value) {
+            print "<option value=\"$value[ID]\">$value[FirstName] $value[LastName] </option>";
+        }
+        ?>
+    </select>
     <label for="inputEmail" class="sr-only">Email адрес</label>
     <input type="email" name="inputEmail" class="form-control" placeholder="Email адрес" required autofocus>
     <label for="inputFirstName" class="sr-only">Имя</label>
     <input type="user_firstname" name="user_firstname" class="form-control" placeholder="Имя" required autofocus>
-    <label for="inputLastName" class="sr-only">Фамилия</label>
-    <input type="user_lasttname" name="user_lastname" class="form-control" placeholder="Фамилия" required autofocus>
     <label for="inputParName" class="sr-only">Отчество</label>
     <input type="user_parname" name="user_parname" class="form-control" placeholder="Отчество" required autofocus>
+    <label for="inputLastName" class="sr-only">Фамилия</label>
+    <input type="user_lasttname" name="user_lastname" class="form-control" placeholder="Фамилия" required autofocus>
     <label for="inputPassword" class="sr-only">Пароль</label>
     <input type="password" name="inputPassword" class="form-control" placeholder="Пароль" required>
+
     <button class="btn btn-lg btn-primary btn-block" type="submit">Зарегистрироваться</button>
     <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
 </form>
