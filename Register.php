34e59cf460657e20b92d1262ef1feb788be0b0f3
Evgeniy <nedanon@outlook.com>
2020-03-04 14:13:37 +0300

    Обновил Register.php

--- Register.php
+++ Register.php
@@ -1,85 +1,189 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Пример на bootstrap 4: Checkout - пользовательская форма заказа, показывающая компоненты формы и функции проверки. Версия v4.0.0">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="./css/signin.css" rel="stylesheet">
     <title>Курсы Java. Регистрация</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="form-validation.css" rel="stylesheet">
 </head>
-<body class="text-center">
+
+<body class="bg-light">
+
 <?php
     require ('connect.php');
 
-if (isset($_POST['inputEmail']) && isset($_POST['inputPassword'])){
-        $email = $_POST['inputEmail'];
-        $user_firstname = $_POST['user_firstname'];
-        $user_lasttname = $_POST['user_lastname'];
-        $user_parname = $_POST['user_parname'];
-        $password = $_POST['inputPassword'];
+
+    if (isset($_POST['email']) && isset($_POST['password'])){
+        $email = $_POST['email'];
+        $LastName = $_POST['LastName'];
+        $FirstName = $_POST['FirstName'];
+        $patronymic = $_POST['patronymic'];
+        $password = $_POST['password'];
+        $registered = date("Y-m-d H:i:s");
+        $stage = 1;
         $pass_hash = password_hash($password, PASSWORD_DEFAULT);
-        $is_teacher = 0;
+        $teacher = $_POST['csTeacher'];
+        $InGroup = $_POST['csGroup'];
 
-    /*$quary = "INSERT INTO users (EMAIL, FirstName, LastName, patronymic, InGroup, teacher, stage, registered, pass_hash) VALUES ('$email', '$user_firstname', '$user_lasttname', '$user_parname',
-    , '$pass_hash')";
-    $result = mysqli_query($connection, $quary);
 
-    if ($result){
-        $smsg = "Регистрация прошла успешно";
-    } else {
-        $fsmsg = "Ошибка";
-    }*/
+        //Я всю ночь провозился и так и не понял, как это работает.
+        //INSERT INTO `jc_students` (`ID`, `email`, `FirstName`, `LastName`, `patronymic`, `pass_hash`) VALUES (NULL, 'pupkin@mail.ru', 'Алексей', 'Пупкин', 'Вячеславович', '');
+        $sth = $db->prepare("INSERT INTO `jc_students` (`ID`, `EMAIL`, `FirstName`, `LastName`, `patronymic`, `InGroup`, `teacher`, `stage`, `registered`, `pass_hash`) VALUES (?,?,?,?,?,?,?,?,?,?)");
+        /*$sth = $db->prepare("INSERT INTO `jc_teachers` (`ID`, `email`, `FirstName`, `patronymic`, `LastName`, `pass_hash`) VALUES (?,?,?,?,?,?)");
+        $sth->execute(array('2', 'phil@mail.ru', 'Александр', 'Иванович', 'Быков', ''));*/
+        $sth->execute(array(NULL, $email, $FirstName, $LastName, $patronymic, $InGroup, $teacher, $stage, $registered, $pass_hash));
+        $insert_id = $db->lastInsertId();
+
+        if ($sth){
+            $smsg = "Регистрация прошла успешно";
+        } else {
+            $fsmsg = "Ошибка";
+        }
+        
     }
 ?>
-<form class="form-singup" method="POST" action="Register.php">
-    <img class="mb-4" src="./assets/jde.png" alt="" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Регистрация</h1>
-    <?php if(isset($smsg)){?><div class="alert-success" role="alert"><?php echo $smsg; ?></div><?php }?>
-    <?php if(isset($fsmsg)){?><div class="alert-danger" role="alert"><?php echo $fsmsg; ?></div><?php }?>
-    <label for="csGroup">Группа</label>
-    <select class="form-control" name="csGroup">
-        <?php
-        $sth = $db->prepare("SELECT * FROM jc_groups");
-        $sth->execute();
-        $array = $sth->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($array as $key => $value) {
-            print "<option value=\"$value[ID]\">$value[Name]</option>";
-        }
-        ?>
-    </select>
-    <label for="csGroup">Преподаватель</label>
-    <select class="form-control" name="csTeacher">
-        <?php
-        $sth = $db->prepare("SELECT * FROM jc_teachers");
-        $sth->execute();
-        $array = $sth->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($array as $key => $value) {
-            print "<option value=\"$value[ID]\">$value[FirstName] $value[patronymic] $value[LastName] </option>";
-        }
-        ?>
-    </select>
-    <label for="inputEmail" class="sr-only">Email адрес</label>
-    <input type="email" name="inputEmail" class="form-control" placeholder="Email адрес" required autofocus>
-    <label for="inputFirstName" class="sr-only">Имя</label>
-    <input type="user_firstname" name="user_firstname" class="form-control" placeholder="Имя" required autofocus>
-    <label for="inputParName" class="sr-only">Отчество</label>
-    <input type="user_parname" name="user_parname" class="form-control" placeholder="Отчество" required autofocus>
-    <label for="inputLastName" class="sr-only">Фамилия</label>
-    <input type="user_lasttname" name="user_lastname" class="form-control" placeholder="Фамилия" required autofocus>
-    <label for="inputPassword" class="sr-only">Пароль</label>
-    <input type="password" name="inputPassword" class="form-control" placeholder="Пароль" required>
 
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Зарегистрироваться</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
-</form>
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<div class="container">
+    <div class="py-5 text-center">
+        <img class="d-block mx-auto mb-4" src="./assets/logo.png" alt="" width="97" height="178">
+        <h2>Форма регистрации</h2>
+        <p class="lead">
+    </div>
+
+
+            <form class="needs-validation" method="POST" action="" novalidate>
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="LastName">Фамилия</label>
+                        <input type="text" class="form-control" name="LastName" placeholder="" value="" required>
+                        <div class="invalid-feedback">
+                            Введите фамилию.
+                        </div>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="FirstName">Имя</label>
+                        <input type="text" class="form-control" name="FirstName" placeholder="" value="" required>
+                        <div class="invalid-feedback">
+                            Введите имя.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="patronymic">Отчество</label>
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="patronymic" placeholder="" required>
+                        <div class="invalid-feedback" style="width: 100%;">
+                            Введите отчество.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="inputEmail">Email </label>
+                    <input type="email" class="form-control" name="email" placeholder="you@example.com" required>
+                    <div class="invalid-feedback">
+                        Введите адрес електронной почты.
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="inputPassword">Пароль</label>
+                    <input type="password" class="form-control" name="password" placeholder="" required>
+                    <div class="invalid-feedback">
+                        Введите пароль.
+                    </div>
+                </div>
+
+                <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="csTeacher">Преподаватель</label>
+                            <select class="custom-select d-block w-100" name="csTeacher" required>
+                                <option value="">Выберите...</option>
+                                <?php
+                                $sth = $db->prepare("SELECT * FROM jc_teachers");
+                                $sth->execute();
+                                $array = $sth->fetchAll(PDO::FETCH_ASSOC);
+                                foreach ($array as $key => $value) {
+                                    print "<option value=\"$value[ID]\">$value[FirstName] $value[patronymic] $value[LastName] </option>";
+                                }
+                                ?>
+                            </select>
+                            <div class="invalid-feedback">
+                                Выберите преподавателя.
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 mb-3">
+                            <label for="csGroup">Группа</label>
+                            <select class="custom-select d-block w-100" name="csGroup" required>
+                                <option value="">Выберите...</option>
+                                <?php
+                                $sth = $db->prepare("SELECT * FROM jc_groups");
+                                $sth->execute();
+                                $array = $sth->fetchAll(PDO::FETCH_ASSOC);
+                                foreach ($array as $key => $value) {
+                                    print "<option value=\"$value[ID]\">$value[Name]</option>";
+                                }
+                                ?>
+                            </select>
+                            <div class="invalid-feedback">
+                                Выберите группу.
+                            </div>
+                        </div>
+                </div>
+
+
+                <hr class="mb-4">
+                <button class="btn btn-primary btn-lg btn-block" type="submit">Зарегистрироваться</button>
+
+            </form>
+
+
+    <footer class="my-5 pt-5 text-muted text-center text-small">
+        <p class="mb-1">&copy; 2020 Java Course</p>
+    </footer>
+</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="/docs/4.0/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+<script src="/docs/4.0/assets/js/vendor/popper.min.js"></script>
+<script src="/docs/4.0/dist/js/bootstrap.min.js"></script>
+<script src="/docs/4.0/assets/js/vendor/holder.min.js"></script>
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+        'use strict';
+
+        window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
+</script>-->
 </body>
 </html>
