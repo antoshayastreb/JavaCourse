5dd700e8b7bfc482481de4c5197b760b8e54aae2
Alexander Aldokhin <Delphideveloper@mail.ru>
2020-03-06 21:48:04 +0300

    Сделал сессии

--- LessonFrame.php
+++ LessonFrame.php
@@ -9,9 +9,7 @@ if (!isset($_SESSION['user_id'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Пример на bootstrap 4: Блог - двухколоночный макет блога с пользовательской навигацией, заголовком и содержанием. Версия v4.0.0">
     <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Custom styles for this template -->
@@ -26,7 +24,7 @@ if (!isset($_SESSION['user_id'])) {
         <a class="p-2 text-muted" href="Lesson.php">Во весь экран</a>
         <a class="p-2 text-muted" href="#">Загрузить выполненное задание</a>
         <a class="p-2 text-muted" href="#">Редактировать профиль</a>
-        <a class="p-2 text-muted" href=Enter.php?do=logout">Выход</a>
+        <a class="p-2 text-muted" href="Enter.php?do=logout">Выход</a>
     </nav>
 </div>
 <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
