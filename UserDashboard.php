b5cd53b6a20ad214b038b882d2048c61549274ed
Нариман Намазов <nedanon@outlook.com>
2020-03-31 19:21:15 +0300

    Навбар выглядит нормально

--- UserDashboard.php
+++ UserDashboard.php
@@ -247,20 +247,17 @@
 
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-    <a class="navbar-brand" href="#">Top navbar</a>
+    <a class="navbar-brand" href="#">Java курс</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="UserDashboard.php">Уроки</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
+            <li class="nav-item active">
+                <a class="nav-link" href="UserDashboard.php?do=UDEditProfile">Профиль</a>
             </li>
         </ul>
         <button class="btn btn-outline-danger my-2 my-sm-0" onClick='location.href="Enter.php?do=logout"'>Выход</button>
